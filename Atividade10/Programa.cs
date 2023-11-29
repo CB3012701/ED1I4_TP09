ae338234aa58fab6e4210b8c90356c1bd4babe30
CB3012701 <82848078+CB3012701@users.noreply.github.com>
2023-11-29 13:24:10 -0300

    Create Programa.cs

---
+++ Atividade10/Programa.cs
@@ -0,0 +1,30 @@
+using Atividade09.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade09
+{
+    /*
+* PARTICIPANTES
+* Érica Barbosa Pereira Lobo
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jornada = new Jornada();
+
+            var seletor = new Seletor(jornada);
+
+            int opcao = -1;
+            while (opcao != 0)
+            {
+                opcao = seletor.EscolherOpcao();
+            }
+
+        }
+    }
+}
